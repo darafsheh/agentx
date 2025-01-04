ed3b7066a8107b38661e9181fd00880f4f92927f
darafsheh <shardaraf@gmail.com>
2025-01-04 00:56:49 -0500

    added railway config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: `${process.env.VITE_API_URL}`,
+                target: `http://localhost:${process.env.SERVER_PORT || 3000}`,
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
